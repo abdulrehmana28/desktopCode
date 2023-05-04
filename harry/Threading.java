848151b2e319e5df0839305fc1c28a466b17a921
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-05-04 12:53:00 +0500

    learned basic Threading in java

---
+++ harry/Threading.java
@@ -0,0 +1,30 @@
+package javaCC.harry;
+
+public class Threading {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start(); // start is use to run methods simultaneously & it is in Thread class
+        t2.start(); // if we use run() one Thread1 will run 
+    }
+    
+}
+
+class Thread1 extends Thread { // Thread is a class 
+    @Override
+    public void run() {
+        while (true) {
+        System.out.println("Thread one is running");
+        }
+    }
+}
+
+class Thread2 extends Thread {
+    @Override
+    public void run() {
+        while (true) {
+        System.out.println("Thread two is running");
+        }
+    }
+}
