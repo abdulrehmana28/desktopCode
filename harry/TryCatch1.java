5fab1295ffd06ed6018cd893e6ce4c31b8df2065
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-05-05 11:33:47 +0500

    Basic try Catch

---
+++ harry/TryCatch1.java
@@ -0,0 +1,20 @@
+package javaCC.harry;
+
+
+public class TryCatch1 {
+    public static void main(String[] args) {
+        
+        int number1 = 2;
+        int number2 = 6;
+        float result = 0;
+
+        try {
+            result = number1 / number2;
+            System.out.println("The result is " + result);  //? This is try catch use to handle exceptions
+        }
+        catch(Exception e) {
+            System.out.println("Sorry, we can't get the result: " + e);
+        }
+    }
+    
+}
