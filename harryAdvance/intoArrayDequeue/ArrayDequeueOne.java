3c58fecc4066dd8dff25afd954d0a6e4f4e8cbda
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-07-20 02:35:05 +0500

    Array Dequeue Learning:

---
+++ harryAdvance/intoArrayDequeue/ArrayDequeueOne.java
@@ -0,0 +1,17 @@
+package javaCC.harryAdvance.intoArrayDequeue;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeueOne {
+    public static void main(String[] args) {
+
+        // Array Dequeue is an array in which additional and deletion can be performed form both side of Array.
+
+        ArrayDeque <Integer> ad1 = new ArrayDeque<Integer>();  // syntax is same as LinkedList or ArrayList
+
+        ad1.add(72);    //? many methods like addFirst() , addLast() , getFirst() , getLast()
+
+        //! see documentation for more information
+    }
+    
+}
