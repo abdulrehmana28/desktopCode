6a166244c80b87f37646e6bb1bd3c7be44b65d57
Abdulrehman Azam <abdulrehmana28@skiff.com>
2023-07-31 17:27:49 +0500

    stated dsa

---
+++ dsa/partOne/arrays/ArrayClassFile.java
@@ -0,0 +1,20 @@
+package javaCC.dsa.partOne.arrays;
+
+public class ArrayClassFile {
+    private int[] items;
+    private int count = 0;
+    ArrayClassFile(int length) { 
+        this.length = length;
+    }
+    
+    
+
+    public void insert(){
+        for(int i = 0; i < count; i++){
+            items[i] = items[i]
+        }
+    }
+
+    public void print() {
+    }
+}
---
+++ dsa/partOne/arrays/MainClassFile.java
@@ -0,0 +1,10 @@
+package javaCC.dsa.partOne.arrays;
+
+public class MainClassFile {
+    public static void main(String[] args) {
+        ArrayClassFile numbers = new ArrayClassFile(3);
+        numbers.insert();
+        numbers.print();
+    }
+    
+}
